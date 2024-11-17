e0bc4246da8e0adc1ac39d7a223e311094021fc1
akshay.singhal <akshay.singhal@neosoftmail.com>
2024-11-17 22:43:18 +0530

    strapi

---
+++ config/plugins.ts
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: 'dwbojz9lo',
+          api_key: '777477156855391',
+          api_secret: 'tr3hyCczUjNCZUH29NcMvsyZaa8',
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
